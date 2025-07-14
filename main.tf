4df84103e5b0df6811504c2551badd834a9bc1bb
Amolak-ai <amolvakotkar@gmail.com>
2025-07-14 18:17:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,19 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "pipeline-rg"
+  name     = "zhingat-rg"
   location = "West Europe"
 }
 
+
 resource "azurerm_storage_account" "stg" {
-  name                     = "amswstg"
+  name                     = "pushpa-pipeline"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_storage_account" "stg" {
+  name                     = "rajjoo-pipeline"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
