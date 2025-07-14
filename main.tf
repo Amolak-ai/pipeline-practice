ece8683c99edeaba58ac926c0cb9f70ef1238a21
Amolak-ai <amolvakotkar@gmail.com>
2025-07-14 18:36:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,15 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "anmswarg"
+  name     = "vibhishanrg00982"
   location = "West Europe"
 }
 
 
 resource "azurerm_storage_account" "stg" {
-  name                     = "anmswaapipe"
+  name                     = "anmswaapip20898"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
 
-resource "azurerm_storage_account" "stg2" {
-  name                     = "anmswaaaapipe"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-}
+
